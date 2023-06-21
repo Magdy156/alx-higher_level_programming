ff5cafcdf18f1df85796aa7ab0d1d7d1acb9617c
Magdy156 <magdyhamdy156@gmail.com>
2023-06-21 19:57:27 +0300

    task 11 modified

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     copy = my_list.copy()
-    my_list.clear()
     if idx < 0 or idx > len(copy) - 1:
         return (my_list)
+    my_list.clear()
     for i in range(len(copy)):
         if i != idx:
             my_list.append(copy[i])
