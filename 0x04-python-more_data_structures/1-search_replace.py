d1874ca815fd4f96bef2b429f5fabd58538c3278
Magdy156 <magdyhamdy156@gmail.com>
2023-06-24 00:18:49 +0300

    task 1 modifi

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     copy = my_list.copy()
     for i in range(len(my_list)):
-        if (copy[i] == 2):
-            copy[i] = 89
+        if (copy[i] == search):
+            copy[i] = replace
     return (copy)
