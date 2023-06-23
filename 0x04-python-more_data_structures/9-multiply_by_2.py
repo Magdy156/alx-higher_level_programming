23755b3f6e4a20474c0f805ec46d3bcda4345837
Magdy156 <magdyhamdy156@gmail.com>
2023-06-24 02:47:23 +0300

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    copy = a_dictionary.copy()
+    keys = list(copy.keys())
+    
+    for key in keys:
+        copy[key] *= 2
+    return (copy)
