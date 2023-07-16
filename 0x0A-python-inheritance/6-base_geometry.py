d54bb8c26016e405cd538503b8756e2936efd974
Magdy156 <magdyhamdy156@gmail.com>
2023-07-16 13:34:01 +0300

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define an empty class"""
+
+
+class BaseGeometry:
+    """Represent base geometery"""
+    def area(self):
+        """will not be implemented"""
+        raise Exception("area() is not implemented")
