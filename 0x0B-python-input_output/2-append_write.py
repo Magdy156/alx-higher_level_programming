fb544eca851d651ae45b1616d486b935a1633ffc
Magdy156 <magdyhamdy156@gmail.com>
2023-07-17 02:08:45 +0300

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a file-appending function."""
+
+
+def write_file(filename="", text=""):
+    """append a string to a UTF8 text file.
+    Args:
+        filename (str): file name
+        text (str): text to be written
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
