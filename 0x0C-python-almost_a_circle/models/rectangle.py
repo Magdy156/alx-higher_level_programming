937508f21a2de53c81a44707e9c2ad4a885ff7bb
Magdy156 <magdyhamdy156@gmail.com>
2023-07-20 14:49:04 +0300

    2,3 modified

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,10 +21,10 @@ class Rectangle(Base):
             ValueError: If either of width or height <= 0.
             ValueError: If either of x or y < 0.
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
