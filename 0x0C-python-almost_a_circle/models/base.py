dfb614ab2f7843845f85ddb3a1a14df9c641ec3b
Magdy156 <magdyhamdy156@gmail.com>
2023-07-21 22:01:25 +0300

    task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -55,3 +55,18 @@ class Base:
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return  an instance with all attributes already set
+
+        Args:
+            **dictionary (dict): Key/value pairs to initialize
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
