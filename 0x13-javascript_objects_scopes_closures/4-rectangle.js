680bc543c8a2b4fc13e857333f4cc18e7ce8bb6f
Magdy156 <magdyhamdy156@gmail.com>
2023-09-12 19:04:29 +0300

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let width = '';
+      for (let j = 0; j < this.width; j++) {
+        width += 'X';
+      }
+      console.log(width);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
