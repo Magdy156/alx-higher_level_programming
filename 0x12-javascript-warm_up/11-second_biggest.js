3de9547a9eab56e10ac6fc02258db870b4f18b72
Magdy156 <magdyhamdy156@gmail.com>
2023-09-12 11:01:09 +0300

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
+const elements = [];
 if (process.argv.length <= 3) {
   console.log(0);
 }
-const elements = process.argv.sort();
+for (let i = 2; i < process.argv.length; i++) {
+  elements.push(parseInt(process.argv[i]));
+}
+elements.sort((a, b) => a - b);
 console.log(elements[elements.length - 2]);
